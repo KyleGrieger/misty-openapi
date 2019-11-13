2aed6b952879c8dfe635be2f99c574b54870c483
KyleGrieger <kyle.grieger@gmail.com>
2019-11-13 15:00:21 -0700

    update server code

---
+++ server/api.py
@@ -0,0 +1,25 @@
+import connexion
+
+def drive_get(**kwargs):
+    # do something
+    print(kwargs)
+    return 'Not implemented', 200
+
+def drive_post(**kwargs):
+    # do something
+    print(kwargs)
+    return 'Not implemented', 200
+
+def drive_stop_get(**kwargs):
+    # do something
+    print(kwargs)
+    return 'Not implemented', 200
+
+def drive_stop_post(**kwargs):
+        # do something
+        print(kwargs)
+        return 'Not implemented', 200 
+
+#app = connexion.FlaskApp(__name__)
+#app.add_api('../reference/misty-api.v1.yaml', resolver=RestyResolver('api'))
+#app.run(server='tornado', port=3000)
